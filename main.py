4faaafbb5e4417397cea8914b17f8aac17c56a38
sayan629 <sayanpal771@gmail.com>
2026-08-24 01:48:46 +0530

    FastAPI working..

--- main.py
+++ main.py
@@ -31,19 +31,19 @@ def greet():
 @app.post('/predict')
 def predict(data : StudentData):
     input_row = pd.DataFrame([{
-        'Age', 
-        'Gender',
-        'Country',
-        'Academic_Level',
-        'Most_Used_Platform',
-        'Purpose_Of_Use',
-        'Avg_Daily_Usage_Hours',
-        'Daily_Unlocks',
-        'Study_Hours',
-        'Physical_Activity_Hours',
-        'Sleep_Hours_Per_Night',
-        'Stress_Level',
-        'Mental_Health_Score',
-        'Grouped_country'
-    }])
+        'Age'                       :data.age,
+        'Gender'                    :data.gender,
+        'Country'                   :data.country,
+        'Academic_Level'            :data.academic_level,
+        'Most_Used_Platform'        :data.most_used_platform,
+        'Purpose_Of_Use'            :data.purpose_of_use,
+        'Avg_Daily_Usage_Hours'     :data.avg_daily_usage_hours,
+        'Daily_Unlocks'             :data.daily_unlocks,
+        'Study_Hours'               :data.study_hours,
+        'Physical_Activity_Hours'   :data.physical_activity_hours,
+        'Sleep_Hours_Per_Night'     :data.sleep_hours_per_night,
+        'Stress_Level'              :data.stress_level,
+        'Grouped_country' 
+   }])
+
     
