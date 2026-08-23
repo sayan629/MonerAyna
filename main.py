7031abf42ba43e9a4ac27382bda7b8031e136361
sayan629 <sayanpal771@gmail.com>
2026-08-24 01:40:46 +0530

    FastAPI working..

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import joblib
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 model = joblib.load('Mental_Health_Model.pk1')
 
 app = FastAPI()
+
+
+# First Pydantic model
+class StudentData(BaseModel):
+    age                     : int = Field(..., ge=10, le=100)
+    gender                  : Literal['Male', 'Female']
+    country                 : str
+    academic_level          : Literal['Undergraduate', 'Graduate', 'High School']
+    most_used_platform      : Literal['Facebook', 'LinkedIn', 'Instagram', 'Snapchat','Twitter','YouTube', 'TikTok', 'LINE', 'KakaoTalk', 'VKontakte', 'WhatsApp','WeChat']
+    purpose_of_use          : Literal['Networking', 'Education', 'Entertainment', 'News']
+    avg_daily_usage_hours   : float = Field(..., ge=0, le=24)
+    daily_unlocks           : int   = Field(..., ge=0)
+    study_hours             : float = Field(..., ge=0, le=24)
+    physical_activity_hours : float = Field(..., ge=0, le=24)
+    sleep_hours_per_night   : float = Field(..., ge=0, le=24)
+    stress_level            : Literal['Medium', 'Low', 'Very High', 'High']  
+    
 
 @app.get('/')
 def greet():
