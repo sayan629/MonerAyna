bbb5183094f505dcce74eee4b87a222a389166ed
sayan629 <sayanpal771@gmail.com>
2026-08-24 01:37:19 +0530

    FastAPI working..

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ app = FastAPI()
 @app.get('/')
 def greet():
     return {'Welcome Sayan'}
+
+@app.post('/predict')
+def predict(data : dict):
+    input_row = {
+        'Age', 
+        'Gender',
+        'Country',
+        'Academic_Level',
+        'Most_Used_Platform',
+        'Purpose_Of_Use',
+        'Avg_Daily_Usage_Hours',
+        'Daily_Unlocks',
+        'Study_Hours',
+        'Physical_Activity_Hours',
+        'Sleep_Hours_Per_Night',
+        'Stress_Level',
+        'Mental_Health_Score',
+        'Grouped_country'
+    }
